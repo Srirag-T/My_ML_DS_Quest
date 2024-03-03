b2ff35c95cd0523b4f3bfa3a92a9ab3d36c93557
Srirag Thangellapally <85727794+Srirag-T@users.noreply.github.com>
2024-03-03 12:44:53 +0530

    Add files via upload

---
+++ SupportModule.py
@@ -0,0 +1,12 @@
+def sq(lst):
+    return [i**2 for i in lst]
+def cub(lst):
+    return [i**3 for i in lst]
+def subm3(a=0,b=0):
+    return a+b, a-b, a*b
+def greet():
+    print('Hello')
+def greet2(n):
+    print('Hello',n)
+def greet3(n='NA'):
+    print('Hello',n)
